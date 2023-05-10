55c93c997dab28af309168d9759b4d42268741c6
Sahil9214 <utkarshsinghal369@gmail.com>
2023-05-10 11:00:19 +0530

    portfolio added

--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -1,61 +1,113 @@
-import React from "react";
-import { Text, Heading, Box, Flex, Image, Divider } from "@chakra-ui/react";
-import "../Styles/Github.css";
-import GitHubCalendar from "react-github-calendar";
-import calender from "../Images/calender.png";
+// import React from "react";
+// import { Text, Heading, Box, Flex, Image, Divider } from "@chakra-ui/react";
+// import "../Styles/Github.css";
+// import GitHubCalendar from "react-github-calendar";
+// import calender from "../Images/calender.png";
 
-const Github = () => {
+// const Github = () => {
+//   return (
+//     <div className="gitStats">
+//       <Text className="github_contribution">GITHUB CONTRIBUTION</Text>
+//       <br />
+//       <Divider width={"80%"} margin={"auto"} />
+//       <br />
+//       <Box>
+//         <Box className="Image_Section" id="github-streak-stats">
+//           <Image
+//             className="githubImages"
+//             w={"100%"}
+//             src="https://github-readme-streak-stats.herokuapp.com/?user=Sahil9214&theme=radical&hide_border=false"
+//             alt="github"
+//           />
+//         </Box>
+//         <br />
+//         <br />
+//         <Box className="Image_Section">
+//           <Flex>
+//             <Image
+//               id="github-top-langs"
+//               width={"50%"}
+//               className="githubImages"
+//               src="https://github-readme-stats.vercel.app/api/top-langs/?username=Sahil9214&theme=radical&hide_border=false&include_all_commits=false&count_private=false&layout=compact"
+//               alt="git"
+//             />
+//           </Flex>
+//         </Box>
+//         <br />
+//         <br />
+//         <Box className="Image_Section">
+//           <Image
+//             id="github-stats-card"
+//             className="githubImages"
+//             w={"70%"}
+//             src="https://github-readme-stats.vercel.app/api?username=Sahil9214&theme=radical&hide_border=false&include_all_commits=true&count_private=true"
+//             alt="contri"
+//           />
+//         </Box>
+
+//       </Box>
+//     </div>
+//   );
+// };
+
+// export default Github;
+import React from "react";
+// import GitHubCalendar from "react-github-calendar";
+import styles from "../Styles/Github.module.css";
+import calender from "../Images/calender.png";
+import { Text, Divider } from "@chakra-ui/react";
+const GitStats = () => {
   return (
-    <div className="gitStats">
-      <Text className="github_contribution">GITHUB CONTRIBUTION</Text>
+    <div className={styles.gitStats}>
+      {/* Heading */}
+      {/* <div className={styles.headingContainer}>
+        <span className={styles.orangeDash}></span>
+        <h2 className={styles.heading}>GITHUB CONTRIBUTIONS</h2>
+        <span className={styles.orangeDash}></span>
+      </div> */}
+      <Text className={styles.github_contribution}>GITHUB CONTRIBUTION</Text>
       <br />
       <Divider width={"80%"} margin={"auto"} />
       <br />
-      <Box>
-        <Box className="Image_Section" id="github-streak-stats">
-          <Image
-            className="githubImages"
-            w={"100%"}
-            src="https://github-readme-streak-stats.herokuapp.com/?user=Sahil9214&theme=radical&hide_border=false"
-            alt="github"
-          />
-        </Box>
-        <br />
-        <br />
-        <Box className="Image_Section">
-          <Flex>
-            <Image
-              id="github-top-langs"
-              width={"50%"}
-              className="githubImages"
-              src="https://github-readme-stats.vercel.app/api/top-langs/?username=Sahil9214&theme=radical&hide_border=false&include_all_commits=false&count_private=false&layout=compact"
-              alt="git"
-            />
-          </Flex>
-        </Box>
-        <br />
-        <br />
-        <Box className="Image_Section">
-          <Image
-            id="github-stats-card"
-            className="githubImages"
-            w={"70%"}
-            src="https://github-readme-stats.vercel.app/api?username=Sahil9214&theme=radical&hide_border=false&include_all_commits=true&count_private=true"
-            alt="contri"
-          />
-        </Box>
-        <Image
-          style={{ border: "10px solid red", zIndex: "1" }}
-          className="react-activity-calendar"
-          src={calender}
-          alt="calender"
+      {/* Calendar */}
+      <div className={styles.calendar}>
+        <img src={calender} alt="" height={"100%"} width={"99%"} />
+        {/* <GitHubCalendar
+          style={{ transform: "scale(18)" }}
+          hideTotalCount="false"
+          hideColorLegend="false"
+          blockSize={15}
+          username="drahul97"
+        /> */}
+      </div>
+      {/* Stats */}
+      <div className={styles.commits}>
+        <img
+          id="github-streak-stats"
+          className={styles.commit2}
+          src="https://github-readme-streak-stats.herokuapp.com/?user=Sahil9214&theme=radical&hide_border=false"
+          // src="https://github-readme-streak-stats.herokuapp.com?user=dRahul97&theme=radical&hide_border=false"(previous one)
+          // [![GitHub Streak](https://github-readme-streak-stats.herokuapp.com?user=dRahul97&theme=radical&hide_border=true)](https://git.io/streak-stats)
+          alt=""
         />
-       
-        <GitHubCalendar username="sahil9214" />
-     
-      </Box>
+
+        <img
+          id="github-top-langs"
+          className={styles.commit3}
+          src="https://github-readme-stats.vercel.app/api/top-langs/?username=Sahil9214&theme=radical&hide_border=false&include_all_commits=false&count_private=false&layout=compact"
+          // src="https://github-readme-stats.vercel.app/api/top-langs/?username=dRahul97&theme=radical&hide_border=false&include_all_commits=false&count_private=false&layout=compact"(previous one)
+          alt=""
+        />
+        <img
+          id="github-stats-card"
+          className={styles.commit1}
+          src="https://github-readme-stats.vercel.app/api?username=Sahil9214&theme=radical&hide_border=false&include_all_commits=true&count_private=true"
+          // src="https://github-readme-stats.vercel.app/api?username=dRahul97&theme=radical&hide_border=false&include_all_commits=true&count_private=true"(previous one)
+          alt=""
+        />
+      </div>
     </div>
   );
 };
 
-export default Github;
+export default GitStats;
